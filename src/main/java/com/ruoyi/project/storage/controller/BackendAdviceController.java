ce3d1ae33796271b2082614fb6e5aeac4469617b
HaiJaine <2215654043@qq.com>
2021-03-12 09:11:53 +0800

    测试返回集合

--- src/main/java/com/ruoyi/project/storage/controller/BackendAdviceController.java
+++ src/main/java/com/ruoyi/project/storage/controller/BackendAdviceController.java
@@ -38,15 +38,19 @@ public class BackendAdviceController extends BaseController {
         Order order1 = new Order();
         Order order2 = new Order();
         Order order3 = new Order();
+        Order order4 = new Order();
         order1.setOrderName("张三");
         order2.setOrderName("李四");
         order3.setOrderName("王五");
+        order4.setOrderName("赵六");
         order1.setUpdateTime(new Date());
         order2.setUpdateTime(new Date());
         order3.setUpdateTime(new Date());
+        order4.setUpdateTime(new Date());
         orders.add(order1);
         orders.add(order2);
         orders.add(order3);
+        orders.add(order4);
         return orders;
     }
 }
