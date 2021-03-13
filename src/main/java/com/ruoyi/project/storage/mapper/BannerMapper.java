e37a9d85b29eb3785e5a355fff966037c5cf8a9f
HaiJaine <2215654043@qq.com>
2021-03-13 09:06:25 +0800

    广告管理

---
+++ src/main/java/com/ruoyi/project/storage/mapper/BannerMapper.java
@@ -0,0 +1,10 @@
+package com.ruoyi.project.storage.mapper;
+
+import com.ruoyi.project.storage.domain.BannerVO;
+
+import java.util.List;
+
+public interface BannerMapper {
+
+    List<BannerVO> findBannerList();
+}
